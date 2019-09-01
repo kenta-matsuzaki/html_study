efde9ae3c90aa531046c32b838384a1af1d664a8
kenta-matsuzaki <kenta6910@gmail.com>
2019-09-01 22:47:19 +0900

    Update main.js
    jsでメニューを開閉できるようにする

--- html_2_forSmartphoneSite/js/main.js
+++ html_2_forSmartphoneSite/js/main.js
@@ -1,7 +1,15 @@
 'use strict';
 
 {
-  document.getElementById('open_menu').addEventListener('click', ()=> {
-    alert('click');
+  const openMenu = document.getElementById('open_menu');
+  const menu = document.getElementById('menu');
+  const closeMenu = document.getElementById('close_menu');
+  
+  openMenu.addEventListener('click', ()=> {
+    menu.classList.add('shown');
+  });
+
+  closeMenu.addEventListener('click', ()=> {
+    menu.classList.remove('shown');
   });
 }
